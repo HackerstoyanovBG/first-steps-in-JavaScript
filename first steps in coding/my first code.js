5b9cfa586d8af658e5943551bceff929f909987f
Hacker <hackerstoanov@gmail.com>
2023-01-07 17:45:38 +0100

    added my first code

---
+++ first steps in coding/my first code.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(700, 550);
+}
+
+function draw() {
+  background(220);
+  fill(250, 250, 250);
+  rect(0, 0, 700, 200);
+  fill(250, 0, 0);
+  rect(0, 380, 700, 200);
+  fill(0, 115, 0);
+  rect(0,180, 700, 200);
+}
